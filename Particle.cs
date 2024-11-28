f1f605a34cb5017087ce1d843d1748f8870a1560
FlakeyCheese <rgblackett@gmail.com>
2024-11-28 15:36:33 +0000

    circles added

--- Particle.cs
+++ Particle.cs
@@ -14,7 +14,7 @@ namespace Particles_V1._0
         public int time_to_death = 50;//number of steps to death. This is used to calculate the fade out on the particle so don'tmess with it too much
         public Vector2 particlePosition;//particles position
         public  Vector2 vector;     //vector representing direction and speed
-        public Vector2 environment =new Vector2(0.0f, 0.0f);//a vector that can be used for wind or gravity. Try (0f, 0.25f) fro gentle gravity
+        public Vector2 environment =new Vector2(0.0f, 0.250f);//a vector that can be used for wind or gravity. Try (0f, 0.25f) fro gentle gravity
                                                         // or (-0.5f, -0.5f) for a wing blowing towards the top left of the screen
         public int d =5;//diameter of a particle less works...more does not
                 
